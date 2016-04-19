2c79ca54b2e319ce4717bbfecbbe322b35d40dc2
michaelblayney <michael.blayney26@gmail.com>
2016-04-19 12:52:31 +1200

    Added in basic structures for code
    Included the libraries
    Included some basic functions that we will need
    Set up basic 'main'

--- AVC_Code.c
+++ AVC_Code.c
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+extern "C" int InitHardware();
+extern "C" int ReadAnalog(int ch_adc);
+extern "C" int Sleep(int sec, int usec);
+extern "C" int SetMotor(int motor ,int dir,int speed);
 
+int main()
+{
+  
+}
