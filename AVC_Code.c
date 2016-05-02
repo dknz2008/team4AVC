06bfc86d1511be56e78e1c2d69b99f1835473ef5
michaelblayney <michael.blayney26@gmail.com>
2016-05-03 11:04:29 +1200

    Updated code for testing
    Wrote chunks of code for testing in the next lab. Code not intended to be run as is, just sections of it

--- AVC_Code.c
+++ AVC_Code.c
@@ -8,5 +8,61 @@ extern "C" int SetMotor(int motor ,int dir,int speed);
 
 int main()
 {
+  InitHardware();
+  int adc_reading = 0;
   
+  //Drives forward and stops abruptly if something is in front of it
+  while (adc_reading < 600)
+  {
+    adc_reading = ReadAnalog(0);
+    SetMotor(1, 1, 255);
+    SetMotor(2, 1, 255);
+    //Sleep(0,500000);
+  }
+  
+  //Turns left
+  SetMotor(1, 0, 255);
+  SetMotor(2, ,1 255);
+  
+  //Turns left
+  SetMotor(1, 0, 255);
+  SetMotor(2, ,1 255);
+  
+  //Turns left until there are no obstructions
+  while (adc_reading >= 600)
+  {
+    adc_reading = ReadAnalog(0);
+    SetMotor(1, 0, 255);
+    SetMotor(2, ,1 255);
+    //Sleep(0,500000);
+  }
+  
+  //Turns right until there are no obstructions
+  while (adc_reading >= 600)
+  {
+    adc_reading = ReadAnalog(0);
+    SetMotor(1, 1, 255);
+    SetMotor(2, ,0 255);
+    //Sleep(0,500000);
+  }
+  
+  //Drives forward and slows down when it gets close to a wall
+   while (adc_reading >= 600)
+  {
+    adc_reading = ReadAnalog(0);
+    SetMotor(1,1,adc_reading/700*255);
+    //Sleep(0,500000);
+  }
+  
+  //Gentle stop
+  int count = 255;
+  while (count > 0)
+  {
+    SetMotor(1, 0, count);
+    SetMotor(2, ,1 count);
+    count = count -10;
+    //sleep(0,500000)''
+  }
+  
+  return 0;
 }
