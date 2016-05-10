07cae671b5975d91bf44fc12e6844d7d3a93b6a6
michaelblayney <michael.blayney26@gmail.com>
2016-05-10 12:04:32 +1200

    Updated Code and fixed name mistake
    (Pre-test code) Updated code to get ready for testing and changed name back to AVC_Code.c

--- C Code for Robot/AVC_Code.c
+++ C Code for Robot/AVC_Code.c
@@ -3,7 +3,7 @@
 extern "C" int InitHardware();
 extern "C" int ReadAnalog(int ch_adc);
 extern "C" int Sleep(int sec, int usec);
-extern "C" int SetMotor(int motor ,int dir,int speed);
+extern "C" int set_motor(int motor,int speed);
 extern "C" int take_picture();
 extern "C" char get_pixel(int row,int col,int colour);
 extern "C" int update_screen();
@@ -14,22 +14,19 @@ extern "C" int close_screen_stream();
 int main()
 {
 
-  int arrayOfPixels[320]; // making array of pixels to store values in 
+  int arrayOfPixels[320]; // making array of pixels to store values in
 
   while(true){
 
       InitHardware();
 
       int adc_reading = 0;
-      
+
       open_screen_stream(); // Puts the video output on desktop screen
 
 		take_picture();
-
-		float kp = 0.5;
 		int sum = 0;
 		int i, w, s;
-
 		for(i=0, i<320, i++){
 			w = get_pixel(i,120,3);
 			if(w>127){
@@ -41,21 +38,29 @@ int main()
 			sum = sum + (i-160)*s;
 		}
 
-		double proportional_signal = sum*kp;
+		if(sum < 0){
+			//turn_right();
+			set_motor(1, 255);
+  			set_motor(2, -255);
 
+		}
 
-		set_motor(1, proportional_signal);
-  		set_motor(2, -1*proportional_signal);
+		if(sum > 0){
+			//turn_left();
 
+  			set_motor(1, -255);
+  			set_motor(2, 255);
+		}
+		if(sum == 0){
+			//drive_forward();
+			set_motor(1, 255);
+  			set_motor(2, 255);
+		}
 
-      update_screen(); //  - updates video output area of the screen. 
+      update_screen(); //  - updates video output area of the screen.
   }
 
   close_screen_stream()
-  
+
   return 0;
 }
-
-
-
-
