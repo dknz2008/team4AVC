dfdcefe44e86240cc7faaec448f308406b559563
michaelblayney <michael.blayney26@gmail.com>
2016-05-17 12:22:58 +1200

    Fixed code after changing it in command line
    Code works correctly after testing

--- C Code for Robot/StraightLineCode.C
+++ C Code for Robot/StraightLineCode.C
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <time.h>
-extern "C" int InitHardware();
-extern "C" int Sleep(int sec, int usec);
+extern "C" int init(int);
+extern "C" int sleep(int sec, int usec);
 extern "C" int set_motor(int motor,int speed);
-
 
 int main()
 {
-
-  set_motor(0, 100);
-  set_motor(1, 100);
-
+  init(0);
+  set_motor(1, -100);
+  set_motor(2, -100);
+  sleep(3,0);
+  set_motor(1, 0);
+  set_motor(2, 0);
   return 0;
 }
-//test
