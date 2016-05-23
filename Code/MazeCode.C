b87f7a661732e85998cae3f28a9816d29af7b98c
michaelblayney <michael.blayney26@gmail.com>
2016-05-24 11:52:51 +1200

    Updated MazeCode.C for maze detection

--- Code/MazeCode.C
+++ Code/MazeCode.C
@@ -11,7 +11,6 @@ int main (){
 	//Variables
 	int tooClose = 500;
 	int wallDistance = 200;
-	int detected = 80; //variable 'detected' used to detect if the robot is entering the maze 
 	int speed = 100;
 	int adc_Front = 0;
 	int adc_Left = 0;
@@ -20,70 +19,73 @@ int main (){
 	int usectorTime = 0;
 	int turnTime = 1;
 	int uturnTime = 0;
-	
+	String state = "line";
 	
 	init(0);
-	while(1)
+	if(state == maze) //State will be changed from "line" to "maze" when the camera detects an amount of red pixels
 	{
-		adc_Front = ReadAnalog(1);
-		adc_Left = ReadAnalog(2);
-		adc_Right = ReadAnalog(3);
-		
-		//printf("%d\n", adc_readingFront);
-		
-		//Through testing i have found that the way to complete any maze
-		if(adc_Front < wallDistance)
+		while(1)
 		{
-			if(adc_Left < wallDist)
-			{
-				//90 Degree turn left
-				set_motor(1,-1 *speed);
-				set_motor(2,speed);
-				Sleep(turnTime,uturnTime);
-				//Then drive forward one sector
-				set_motor(1,speed);
-				set_motor(2,speed);
-				Sleep(sectorTime,usectorTime);
-			}
-			else
+			adc_Front = ReadAnalog(1);
+			adc_Left = ReadAnalog(2);
+			adc_Right = ReadAnalog(3);
+			
+			//printf("%d\n", adc_readingFront);
+			
+			//Through testing i have found that the way to complete any maze
+			if(adc_Front < wallDistance)
 			{
 				if(adc_Left < wallDist)
 				{
-					//Drive forward one sector
+					//90 Degree turn left
+					set_motor(1,-1 *speed);
+					set_motor(2,speed);
+					Sleep(turnTime,uturnTime);
+					//Then drive forward one sector
 					set_motor(1,speed);
 					set_motor(2,speed);
 					Sleep(sectorTime,usectorTime);
 				}
+				else
+				{
+					if(adc_Left < wallDist)
+					{
+						//Drive forward one sector
+						set_motor(1,speed);
+						set_motor(2,speed);
+						Sleep(sectorTime,usectorTime);
+					}
+				
+			}
+			else if(adc_Front > tooClose) //If there is a wall infront of the robot
+			{
+				if(adc_Left >= wallDistance && adc_Right >= wallDistance) //If the robot comes to a dead end
+				{
+					//180 Degree turn
+					set_motor(1,-1* speed);
+					set_motor(2,100);
+					Sleep(turnTime,uturnTime);
+				}
+				else if(adc_Left >= wallDistance) //If the robot comes to a place where there is walls infront and to the left but not to the right
+				//Note at a T intersection 
+				{
+					//90 Degree turn right
+					set_motor(1,speed);
+					set_motor(2,-1 * speed);
+					Sleep(turnTime,uturnTime);
+				}
+				else //If the robot comes to a place where there is walls infront but no wall to the left, it will turn left
+				//Note it will make the robot turn left at a T intersection
+				{
+					//90 Degree turn left
+					set_motor(1,-1 * speed);
+					set_motor(2,speed);
+					Sleep(turnTime,uturnTime);
+				}
+			}
 			
+			Sleep(1,0);
 		}
-		else if(adc_Front > tooClose) //If there is a wall infront of the robot
-		{
-			if(adc_Left >= wallDistance && adc_Right >= wallDistance) //If the robot comes to a dead end
-			{
-				//180 Degree turn
-				set_motor(1,-1* speed);
-				set_motor(2,100);
-				Sleep(turnTime,uturnTime);
-			}
-			else if(adc_Left >= wallDistance) //If the robot comes to a place where there is walls infront and to the left but not to the right
-			//Note at a T intersection 
-			{
-				//90 Degree turn right
-				set_motor(1,speed);
-				set_motor(2,-1 * speed);
-				Sleep(turnTime,uturnTime);
-			}
-			else //If the robot comes to a place where there is walls infront but no wall to the left, it will turn left
-			//Note it will make the robot turn left at a T intersection
-			{
-				//90 Degree turn left
-				set_motor(1,-1 * speed);
-				set_motor(2,speed);
-				Sleep(turnTime,uturnTime);
-			}
-		}
-		
-		Sleep(1,0);
 	}
 	
 	return 0;
