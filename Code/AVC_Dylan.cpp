b983bf4b49ac766b9e662210a9cdf52f41845726
dknz2008 <dylan.kumar@icloud.com>
2016-05-24 13:14:35 +1200

    Networking code
    (Dylan), Worked with Ilan and Michael

--- Code/AVC_Dylan.cpp
+++ Code/AVC_Dylan.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <time.h>
 extern "C" int init(int);
@@ -9,13 +10,38 @@ extern "C" char get_pixel(int row,int col,int color);
 extern "C" int take_picture();
 extern "C" int select_IO(int chan, int direct);
 
+extern "C" int connect_to_server(char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+
+
+
+int network(){
+        //network code
+        char ip[20] = "130.195.6.196\0";
+
+        connect_to_server(ip, 1024);
+        printf("Connecting");
+        char msg[20] = "Please\0";
+        send_to_server(msg);
+        printf("something happened 1");
+        char message[24];
+        receive_from_server(message);
+        printf("something happened");
+        send_to_server(message);
+        printf("%s", message);
+        return 0;
+}
+
+
 int main (){
       // This sets up the RPi hardware and ensures
       // everything is working correctly
       init(0);
 
 
-        int msec =0;
+
+      int msec =0;
         int trigger = 10;
         clock_t before = clock();
 
@@ -35,30 +61,14 @@ int main (){
         int seconds_passed = 0;
         int abcd = 0;
 
-        while (abcd < 600) {
 
-        take_picture();
-        float current_error_average = 0;
-                pid_left = 0;
-                pid_right = 0;
 
-                current_error = 0;
-                proportional_signal = 0;
-       float  pid_left;
-        float pid_right;
-        int w, s;
-        int w1; //second line
-        double proportional_signal;
-        float dir = 0;
-        int changeTurn = 0;
-        time_t sec;
-        time_t sec1;
-        sec = time(NULL);
-        sec1 = time(NULL);
-        int seconds_passed = 0;
-        int abcd = 0;
+//      network();
+        printf("NETWORK IS SUCCESSFUL********************** \n");
 
-        while (abcd < 600) {
+
+while (1) {
+
 
         take_picture();
         float current_error_average = 0;
@@ -75,6 +85,8 @@ int main (){
                 for(int i=0; i<320; i++){
                         w = get_pixel(i,120,3);
                         w1 = get_pixel(i, 0, 3); // at the top of the camera (or 239, check $
+                      //  printf("w1: %d  \n", w1);
+                //      printf("w:%d \n", w);
                         if(w>127){
                                 s = 1;
                                 num++;
@@ -89,23 +101,24 @@ int main (){
 
  current_error = (current_error + (i-160)*s);
 
-
-                      }
-                        current_error_average = current_error/num;
+                        }
+  //                      current_error_average = current_error/num;
 
                 //Getting the robot to move
                 proportional_signal = current_error*kd;
 
 //              printf("current error average: %f\n", current_error_average);
-                printf("proportional_signal %f \n", proportional_signal/320);
-                printf("current error: %f \n", current_error);
+//              printf("proportional_signal %f \n", proportional_signal/320);
+//              printf("current error: %f \n", current_error);
+
 
 
 
                 //sec1 = time(NULL);
                 //printf("sec: %ld", sec);
                 //printf("sec1: %ld", sec1);
-                               sec = time(NULL);
+
+                sec = time(NULL);
                 if((sec-sec1) >= 1){
                         sec1 = time(NULL);
                         if(dark1 >=320 && dark >=320){
@@ -114,7 +127,7 @@ int main (){
                                 if(seconds_passed = 1){
                                         dir = current_error;
                                 }
-                                if(seconds_passed >= 3){
+                                if(seconds_passed >= 2){
                                         dir = -current_error;
                                         seconds_passed = 0;
                                 }
@@ -122,63 +135,58 @@ int main (){
 
                 }
 
-                if(num >= 310 && dark1 < 320){
+                if(num >= 320 && dark1 < 320){
                         pid_left = -100;
                         pid_right = -100;
                         Sleep(1, 800000);
                 }else
                  if(dark1 >=320  && dark >=320){
-                //      dir = current_error;
+                        dir = current_error;
 
                         if(dir > 0){
                                 pid_left = (-120); //turn left i think
                                 pid_right = (120);
-                               // Sleep(1, 800);
-                                //changeTurn = 1;
+                                changeTurn = 1;
                         }else if(dir < 0){
- pid_left = (120);
+                                pid_left = (120);
                                 pid_right = -120; //probably turn right
-                                //Sleep(1,800);
-                                //changeTurn = 0;
+
                         }else{
                                 pid_left = -120;
                                 pid_right = 120;
-                        //
-                        } 
+
+                        }
                         //Sleep(1, 400000);
                 }else{
 
-//              if(dark >= 320){
-//                      printf("dark");
-//                      pid_left =  -100;
-//                      pid_right = - 100;
-//                      Sleep(0, 5000);
-//                      dark = 0;
-//              }else{
+pid_left = 80 + proportional_signal/(320);
+                        pid_right = 80 - proportional_signal/(320);
 
-
-                        pid_left =(-proportional_signal/(320));
-                        pid_right =  -proportional_signal/(320);
              }
+                printf("pid right: %f", pid_left);
+                printf("pid left: %f", pid_right);
+                if(pid_left >= 255){
+                        pid_left = -255;
+                }
+                if(pid_right >= 255){
+                        pid_right = -255;
+                }
+                if(pid_left <= -255){
+                        pid_left = 255;
+                }
+                  if(pid_right <= -255){
+                        pid_right = 255;
+                }
+ 
+                set_motor(1, -pid_left);
+                set_motor(2, -pid_right);
 
-                 set_motor(1, -pid_left);
-                 set_motor(2, -pid_right);
 
-/**             if(pid > 255){
-                        set_motor(1,-1*255);
-                        set_motor(2, 255);
-                }else if(pid < -255){
-                        set_motor(1, 255);
-                       set_motor(2,- 1*255);
-                }else{
-                        set_motor(1, -pid);
-                        set_motor(2, pid);
-  }
-        */
-        abcd++;
  }
 
         set_motor(1,0);
         set_motor(2, 0);
 return 0;
 }
+
+
