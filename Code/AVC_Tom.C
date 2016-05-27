517b5252a06c46a33aba067eefc448316b0aa62e
TomEClark <Clarkysharky@hotmail.co.uk>
2016-05-27 19:07:25 +1200

    Update AVC_Tom.C
    New code

--- Code/AVC_Tom.C
+++ Code/AVC_Tom.C
@@ -1,10 +1,10 @@
-#include <iostream>
+/*Cite Jack Moran, he helped me write out and understand what was needed as our current implementation was a cypher */
+
 #include <stdio.h>
 #include <time.h>
 
 //Extern C
-
-//Initilise Hardware
+/* Initilise Hardware*/
 extern "C" int init(int d_lev);
 
 /* Camera Feed */
@@ -22,25 +22,34 @@ extern "C" int Sleep(int sec, int usec);
 extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
- 
-/* Char */
-char internetProtocol[] = "130.195.6.196";
 
-/* Integer*/
+/* Run */
+int write_digital(int chan, char level);
+int read_analog(int ch_adc);
+int select_IO(int chan,int direct);
+
+ 
+//Variables
+
+/* Char */
+char internetProtocol[] = "130.24.96.123";
+char sendMessage[] = "Please";
+
+/* Intager */
 int port = 1024;
 
 /* Double */
 double pixelTotal;
 double pixelAverage;
-double Kp = 0;		//Proportional Value (Keep values in the tens)
-double Kd = 0;		//Derivative Value
-double Ki = 0;		//Integral Value
+double Kp = 0.4;   //Proportional Value (Keep values in the tens)
+double Kd = 0;     //Derivative Value
+double Ki = 0;     //Integral Value
 double left, right;
 
 /* Floats */
-float kpEv;          //Error Value for proportional
-float kdEv;          //Error Value for derivative
-float kiEv;          //Error Value for integral
+float kpEv;   //Error Value for proportional
+float kdEv;   //Error Value for derivative
+float kiEv;   //Error Value for integral
 
 //Calls
 int networking();
@@ -48,61 +57,60 @@ int lineFollow();
 
 //Methods
 int main(){
-	init(0);               //Initialise Rpi Hardware
-	networking();          //Runs Networking Code
+	init(0);       //Initilise Rpi Hardware
+	networking();  //Runs Networking Code
 }
 
 int networking(){
-	char message[24];                // It can accept up to 24 chars in its array
+	char message[24];// It can accept up to 24 chars in its array
 	
-	connect_to_server(internetProtocol, port);   //Connect to the server passing in variables
-    send_to_server("Please");                        //Sends "Please" to the server
+	connect_to_server(internetProtocol, port);//Connect to the server passing in variables
+    send_to_server(sendMessage);//Sends "Please" to the server
     
-    receive_from_server(message);        //Recieves a message & stores in message(Char)
-    send_to_server(message);             //Sends recieved message to the server
+    receive_from_server(message); //Recieves a message & stores in message(Char)
+    send_to_server(message);      //Sends recieved message to the server
     
-    //printf("%s", message);             //Debugging purpose: Prints out message(Char)
+    //printf("%s", message);//Debugging purpose: Prints out message(Char)
 	return 0;
 	lineFollow();	
 }
 
 int lineFollow(){
 	int pixelArray[32];
-	int sample = sizeOf(pixelArray)/sizeOf(int);//Finds the size of the array then divides by the size of an int
+	int sample = sizeof(pixelArray)/sizeof(int);//Finds the size of the array then divides by the size of an int
 	
-	while(true){                                //Forever True until certain event is met
-		kpEv = 0;                           //Proportional ErrorValue = 0
-		pixelTotal = 0;                     //Pixel Totel = 0
-		pixelAverage = 0;                   //PixelAverage = 0
-		take_picture();                     //Takes a picture with the camera
+	open_screen_stream();
+
+	while(true){           //Forever True until certain event is met
+		kpEv = 0;          //Proportional ErrorValue = 0
+		pixelTotal = 0;    //Pixel Totel = 0
+		pixelAverage = 0;  //PixelAverage = 0
+		take_picture();    //Takes a picture with the camera
 		
-		for(int i = 0; i < sample; i++){                     //This proccess
-			pixelArray[i] = get_pixel(i*10, 120, 3);nnnn //Finds the brightness
-			pixelTotal += pixelArray[i];	             //Of pixels in the array
+		for(int i = 0; i < sample; i++){             /*This proccess         */
+			pixelArray[i] = get_pixel(i*10, 120, 3); /*Finds the brightness  */
+			pixelTotal += pixelArray[i];			 /*Of pixels in the array*/
 		}
 		
-		pixelAverage = pixelTotal/sample;//Finds the average brightest pixel
+		pixelAverage = pixelTotal/sample;            //Finds the average brightest pixel
 		
-		for(int i = 0; i < sample; i++){             //Finds if pixel is brighter
-			if(pixelArray[i]>pixelAverage){      //Then the average, a negitive number means turn left
-				kpEv += 10*(i-sample/2);     //Positive number means turn right
+		for(int i = 0; i < sample; i++){             /*Finds if pixel is brighter                                     */
+			if(pixelArray[i]>pixelAverage){          /*Then the average, a negitive number means turn left            */
+				kpEv += (i-sample/2);                /*Positive number means turn right                               */
 			}
 		}
 		
-		//printf("%f\n", kpEv);            //Debugging Purpose: Prints float(%f) proportional error value
+		//printf("%f\n", kpEv);//Debugging Purpose: Prints float(%f) proportional error value
 		
-		left = 40 + (kpEv * kp);           // Determines the new motor speeds to alter direction
-		right = 40 - (kpEv * kp);          // Determines the new motor speeds to alter direction
+		left = 40 + (kpEv * Kp);   // Determines the new motor speeds to alter direction
+		right = 40 - (kpEv * Kp);  // Determines the new motor speeds to alter direction
 		
-		set_motor(1, left);                // Changes the motor speeds to the predetermined values
-		set_motor(2, right);               // Changes the motor speeds to the predetermined values
+		set_motor(1, left);    // Changes the motor speeds to the predetermined values
+		set_motor(2, right);   // Changes the motor speeds to the predetermined values
 		
-		//printf("%d, %d\n", left, right); //Debugging Purpose: Prints double(%d) left, right values
+		//printf("%d, %d\n", left, right);//Debugging Purpose: Prints double(%d) left, right values
 		update_screen();
 	}
-	
+
 	close_screen_stream();
 	return 0;
-	
-	
-}
